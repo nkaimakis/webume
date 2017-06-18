e77c64c1955e3a858f791ef346bfb47451072596
Nick Kaimakis <kaimakis@usc.edu>
2017-06-18 15:24:40 -0700

    fixed contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -2,9 +2,9 @@
 
 $(function() {
 
-    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
+	$("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+		preventSubmit: true,
+		submitError: function($form, event, errors) {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
@@ -17,27 +17,27 @@ $(function() {
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
+            	firstName = name.split(' ').slice(0, -1).join(' ');
             }
+
+
+
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
+            	url: "https://www.enformed.io/3073q2d2",
+            	method: "post",
+            	dataType: "json",
+            	accepts: "application/json",
+            	data: $("#contactForm").serialize(),
+            	cache: false,
+            	success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
+                    .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                    .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
-                        .append('</div>');
+                    .append('</div>');
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
@@ -46,7 +46,7 @@ $(function() {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
+                    .append("</button>");
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
@@ -55,18 +55,18 @@ $(function() {
             });
         },
         filter: function() {
-            return $(this).is(":visible");
+        	return $(this).is(":visible");
         },
     });
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
+	$("a[data-toggle=\"tab\"]").click(function(e) {
+		e.preventDefault();
+		$(this).tab("show");
+	});
 });
 
 
 /*When clicking on Full hide fail/success boxes */
 $('#name').focus(function() {
-    $('#success').html('');
+	$('#success').html('');
 });
