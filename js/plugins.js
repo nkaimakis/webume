b5a50bef6e1fd356a498b69717a62f5e878ee45a
Nick Kaimakis <kaimakis@usc.edu>
2017-11-10 18:20:17 -0800

    circliful bug fixes with update

--- js/plugins.js
+++ js/plugins.js
@@ -1,20 +1,11 @@
 /*global $,alert,consol*/
-													/*Home*/
+/*Home*/
 $(document).ready(function () {
 	'use strict';
-	
-    $('#graph_1,#graph_2,#graph_3,#graph_4,#graph_5,#graph_6').circliful({
-        animationStep: 3,
-        foregroundBorderWidth: 7,
-        backgroundBorderWidth: 7,
-        percent: 75,
-		animateInView: true,
-		textAdditionalCss:'padding-bottom:20px;',
-		textStyle: 'font-size:28px;',
-		fontColor:'#333'
-    });
-		var mixer = mixitup('.pro_image');
-$("li a").click(function(){$('html, body').animate({scrollTop:$("#"+$(this).data('value')).offset().top},1500);});
+	$('#graph_1,#graph_2,#graph_3,#graph_4,#graph_5,#graph_6').circliful();
+
+	var mixer = mixitup('.pro_image');
+	$("li a").click(function(){$('html, body').animate({scrollTop:$("#"+$(this).data('value')).offset().top},1500);});
 	$(".nav li:first-of-type").click(function(){$('html, body').animate({scrollTop:0},500);});
 	$('#ski').mouseover(function(){
 		$('#se_ski').addClass("active").siblings().removeClass("active");
@@ -34,13 +25,12 @@ $("li a").click(function(){$('html, body').animate({scrollTop:$("#"+$(this).data
 	$('.blog').mouseover(function(){
 		$('#se_blo').addClass("active").siblings().removeClass("active");
 	});
-$(".carousel .first .title_head").typed({
-        strings: ["Hi, My Name is Nick Kaimakis"],
-        typeSpeed: 50
-      });
+	$(".carousel .first .title_head").typed({
+		strings: ["Hi, My Name is Nick Kaimakis"],
+		typeSpeed: 50
+	});
 	$(".form-control").click(function(){
-		
 		$(this).css({"background": "#fff"});
 	});
-	});
+});
 $(window).on('load',function(){'use strict';$(".loading .spinner").fadeOut();$(".loading").fadeOut();$("body").css("overflow","auto");});
